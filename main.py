ab0160915f2a7ac2509e8c654a162f216922de44
abacropsEMP-Louis <118208156+abacropsEMP-Louis@users.noreply.github.com>
2023-02-15 11:55:37 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -105,6 +105,9 @@ def Log_out():
 #Recovery of account
 @app.route('/accounts', methods=['GET', 'POST']) 
 def find_email():
+    Email_locate = ""
+
+
     #DESTROY ANY existing session 
     session.clear()
     #This variable change if the email is found under a account
@@ -116,79 +119,98 @@ def find_email():
 
     #Request email from website template 
     email = request.form.get('Email')
-
+    print(email)
+    if (email != None):
     
-    #if the email enter by user is in the database the allow the user to await for recovery password 
-    if (db_Msql.Find_account_email(email, cursor, MySQL) == True):
+        #if the email enter by user is in the database the allow the user to await for recovery password 
+        if (db_Msql.Find_account_email(email, cursor, MySQL) == True):
+            Email_locate = True
         
-        #store the random generated key 6 random numbers
-        generate_key = random.randint(1000000)
-
-        
-        #Create the session for the user
-        session["loggedin"] = False
-        session["email"] = email
-        session["generate"] = generate_key
-        session['account'] = True
-
+            #store the random generated key 6 random numbers
+            generate_key = random.randint(1000000)
 
         
-        #Email user the recovery key
-        mail_config.send_recovery_key(email, app, generate_key)
+            #Create the session for the user
+            session["loggedin"] = False
+            session["email"] = email
+            session["generate"] = generate_key
+            session['account'] = True
 
-        return redirect(url_for('Validate_email'))
-    else:
-        #Flash there is no account under this email 
-        print("Email not in the database ")
+
+        
+            #Email user the recovery key
+            mail_config.send_recovery_key(email, app, generate_key)
+
+            return redirect(url_for('Validate_email'))
+        
+        elif(db_Msql.Find_account_email(email, cursor, MySQL) == False):
+            Email_locate = False
+            #Flash there is no account under this email 
+            print("Email not in the database ")
+            return render_template('Recovery/validator.html', Email_locate=Email_locate)
   
     return render_template('Recovery/validator.html')
 
+
 @app.route('/validate_email', methods=['GET', 'POST'])
 def Validate_email():
-    print()
-    if (str(request.form.get('key')) == str(session["generate"])):
-        session['loggedin'] = True
+    Valid_code = True 
+    key = request.form.get('key')
+    if (key != None):
+        if (str(key) == str(session["generate"])):
+            session['loggedin'] = True
 
 
-        #Redirect to new password 
-        return redirect(url_for('New_password'))
+            #Redirect to new password 
+            return redirect(url_for('New_password'))
 
-    else:
-        #flash message (The key not valide)
-        print(" Invalide key ")
+        else:
+            Valid_code = False
+            #flash message (The key not valide)
+            print(" Invalide key ")
 
-    return render_template('Recovery/validator_email.html', account_found=session["account"], email= str(session["email"])) 
+    return render_template('Recovery/validator_email.html', account_found=session["account"], email= str(session["email"]), Valid_code=Valid_code) 
 
 @app.route('/new_password', methods=['GET' , 'POST'])
 def New_password():
     pass1 = request.form.get('pass')
     pass2 = request.form.get('passw')
-    if (session['loggedin'] == True):
+    if (pass1 != None):
+        if(pass2 != None):
+            if (session['loggedin'] == True):
 
-        #Generate the mysql cursor 
-        cursor = mYSQL.connection.cursor(MySQLdb.cursors.DictCursor)
+                #Generate the mysql cursor 
+                cursor = mYSQL.connection.cursor(MySQLdb.cursors.DictCursor)
 
-        print(pass1)
-        if(pass1 != None):
-        #If Both password are the same 
-            if (pass1 == pass2):
-        
                 print(pass1)
-                print(pass2)
-            #update the database 
-                db_Msql.Update_Password(session["email"],pass2, cursor, mYSQL)
+        
+                #If Both password are the same 
+                if (pass1 == pass2):
+        
+                    print(pass1)
+                    print(pass2)
+                    #update the database 
+                    db_Msql.Update_Password(session["email"],pass2, cursor, mYSQL)
             
-                print("Update successfully")
-                session.clear()
-		
-
-        print("Empty")
+                    print("Update successfully")
+                    session.clear()
+                    return redirect(url_for('Login'))
+                else:
+                    return render_template('Recovery/New_password.html', pass_value= False)
+                            
+            return render_template('Recovery/New_password.html', pass_value= False)
     else:
         # The password is inconsistant
         print("Invalide user ")
+    return render_template('Recovery/New_password.html', pass_value=True)
+    
 
-    return render_template('Recovery/New_password.html')
 
+
+@app.route('/test')
+def test():
+
+    return render_template('test.html')
 
 #Run the application 
 if __name__ == "__main__":
