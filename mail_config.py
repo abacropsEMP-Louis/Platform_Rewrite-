0c1df158e6b1c47886b35f85a194a8f1ad96e5d3
joanfaAbacrops <josefabre.abacrops@gmail.com>
2023-03-13 11:45:54 -0400

    adding file

---
+++ mail_config.py
@@ -0,0 +1,34 @@
+from flask_mail import Mail, Message
+
+
+def get_config(app):
+    mail = Mail()
+    # CONFIGURING THA MAIL PROTOCOL
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    # EMAIL Address used to send the email to the user
+    app.config['MAIL_USERNAME'] = 'confirmation.abacrops@gmail.com'
+    # passsword of MAIL_PASSWORD
+    app.config['MAIL_PASSWORD'] = 'cbct qkqd azml gkxl'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    mail = mail.init_app(app)
+    return mail
+
+
+def send_recovery_key(email, app, Random_key):
+    # Configure the flask plataform to use the parameters define in ["get_config"]
+    mail = get_config(app)
+    message = """ (Max Length)
+		################################
+		Here is the recovery key to change your password 
+		key = {}
+	""".format(Random_key)
+
+    msg = Message(
+        'Platform Recovery Key',
+        body=message,
+        sender='confirmation.abacrops@gmail.com',
+        recipients=[email])
+
+    mail.send(msg)
